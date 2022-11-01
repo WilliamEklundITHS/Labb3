4349c4e598bd4b29d4e75af63f28e48c1b0e53fa
WilliamEklundITHS <william.eklund@iths.se>
2022-11-01 11:30:34 +0100

    senaste med ta bort och visa bokningar

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,55 +1,42 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Runtime.Intrinsics.Arm;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using static Labb3.MainWindow.BookingModel;
+
 namespace Labb3
 {
     public partial class MainWindow : Window
     {
         public ObservableCollection<DateTime> AvailableBookingsList { get; set; }
         public ObservableCollection<BookingModel> BookingsList { get; set; }
+
         public BookingModel bookingModel { get; set; }
+        public BooleanToVisibilityConverter visibilityConverter { get; set; }
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = this; listBox.DataContext = this;
-            listBox.ItemsSource = null; cb1.ItemsSource = null;
-            var jim = new DateTime(2022, 11, 10, 8, 0, 0);
+            DataContext = this;
+            listBox.ItemsSource = null;
+            cb1.ItemsSource = null;
             AvailableBookingsList = new ObservableCollection<DateTime>(Enumerable.Range(8, 12).Select(x => new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day).AddHours(x)));
-            cb1.ItemsSource = AvailableBookingsList; BookingsList = new ObservableCollection<BookingModel>()
+            cb1.ItemsSource = AvailableBookingsList;
+            BookingsList = new ObservableCollection<BookingModel>()
             { new BookingModel("2022-11-02 12:00", "Greg", 3),
                 new BookingModel("2022-11-02 19:00", "Mary", 12),
-                new BookingModel("2022-11-05 17:00", "Jane", 7) }; listBox.ItemsSource = BookingsList;
+                new BookingModel("2022-11-05 17:00", "Jane", 7) };
+            listBox.ItemsSource = BookingsList;
+
+            listBox.Visibility = Visibility.Collapsed;
+
         }
         public int SetSelectedCbItemIndex() => cb1.SelectedIndex = 0;
         public void DatePicker_Changed(object sender, RoutedEventArgs e)
         {
             var parsedDate = DateTime.Parse(dp1.Text);
-            AvailableBookingsList = new ObservableCollection<DateTime>(Enumerable.Range(8, 12).Select(x => new DateTime(parsedDate.Year, parsedDate.Month, parsedDate.Day).AddHours(x)));
+            AvailableBookingsList = new ObservableCollection<DateTime>(Enumerable.Range(8, 12).Select(x => new DateTime(
+                parsedDate.Year,
+                parsedDate.Month,
+                parsedDate.Day).AddHours(x)));
             UpdateDateList();
         }
         private void UpdateDateList()
@@ -62,22 +49,37 @@ namespace Labb3
 
                 if (UpdateAvailableDatesList.Any())
                 {
-                    cb1.ItemsSource = null; cb1.ItemsSource = AvailableBookingsList;
+                    cb1.ItemsSource = null;
+                    cb1.ItemsSource = AvailableBookingsList;
 
                     SetSelectedCbItemIndex();
-
                 }
                 cb1.ItemsSource = null;
-
                 cb1.ItemsSource = UpdateAvailableDatesList.ToList();
 
                 SetSelectedCbItemIndex();
 
             }
         }
+
+        private void ButtonRemove_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = listBox.SelectedItems.OfType<BookingModel>().ToList();
+            if (listBox.SelectedIndex is -1)
+            {
+                MessageBox.Show($"Markera bokning du vill ta bort. Det finns total {BookingsList.Count} att ta bort");
+            }
+            foreach (var booking in selectedItems)
+            {
+                BookingsList.Remove(booking);
+            }
+        }
+        private void ButtonList_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.Visibility = Visibility.Visible;
+        }
         private void ButtonAdd_Click(object sender, RoutedEventArgs e)
         {
-
             var parseBookingTableNumber = int.TryParse(BookingTableNumber.Text, out int num);
 
             if (!parseBookingTableNumber) { return; }
@@ -91,37 +93,30 @@ namespace Labb3
             if (string.IsNullOrWhiteSpace(BookingName.Text)) { return; }
 
             BookingsList.Add(bookingModel);
-
             BookingName.Clear();
-
             BookingTableNumber.Clear();
-
             UpdateDateList();
-
             SetSelectedCbItemIndex();
-
         }
+
         public class BookingModel
         {
-
             public virtual string DateAndTime { get; set; }
 
             public virtual string Name { get; set; }
 
             public virtual int BookingTableNumber { get; set; }
-            public BookingModel()
-            {
-
-                DateAndTime = string.Empty; Name = string.Empty; BookingTableNumber = 0;
-            }
             public BookingModel(string selectedDate, string name, int bookingTableNumber)
             {
-
                 DateAndTime = selectedDate; Name = name; BookingTableNumber = bookingTableNumber;
             }
-
             public override string ToString() { return $"{DateAndTime} {Name} {BookingTableNumber}"; }
+        }
 
+        public class BooleanToVisibilityConverter
+        {
+            public bool Visible { get; set; } = true;
+            public Visibility visibility => Visible ? Visibility.Visible : Visibility.Collapsed;
         }
     }
 }
